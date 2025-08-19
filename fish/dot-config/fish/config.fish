80eec3ff9f6db2fae82945a180823e28e9503fe5
ElmF <elmforestw@proton.me>
2025-08-20 00:21:57 +0800

    fish: run xdg-ninja

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -13,7 +13,21 @@ if status is-interactive
     alias tree="eza --tree"
 end
 
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_STATE_HOME=$HOME/.local/state
+export XDG_CACHE_HOME=$HOME/.cache
 
 export EDITOR=nvim
+export TERMINAL=foot
 export http_proxy=127.0.0.1:1080
 export https_proxy=127.0.0.1:1080
+
+export GNUPGHOME=$XDG_DATA_HOME/gnupg
+export CARGO_HOME=$XDG_DATA_HOME/cargo
+export GOPATH=$XDG_DATA_HOME/go
+
+export HISTFILE=$XDG_STATE_HOME/bash_history
+export PYTHON_HISTORY=$XDG_STATE_HOME/python_history
+
+fish_add_path $XDG_DATA_HOME/cargo/bin
