eaa994084517bfb0021c9d07b56319ff817782bf
ElmF <elmforestw@proton.me>
2025-09-15 11:55:35 +0800

    firefox: use CRLs instead of OCSP

--- firefox/dot-mozilla/firefox/elmf/user.js
+++ firefox/dot-mozilla/firefox/elmf/user.js
@@ -1,3 +1,8 @@
 user_pref("toolkit.telemetry.enabled", false);
 user_pref("identity.fxaccounts.enabled", false);
 user_pref("browser.tabs.closeWindowWithLastTab", false);
+
+// Use CRLs instead of OCSP
+// https://letsencrypt.org/2025/08/06/ocsp-service-has-reached-end-of-life
+user_pref("security.OCSP.enabled", 0);
+user_pref("security.pki.crlite_mode", 2);
