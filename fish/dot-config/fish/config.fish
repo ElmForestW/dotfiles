0897d032e02a699c74182ad2b65f86cccc00b9eb
ElmF <elmforestw@proton.me>
2025-10-18 12:19:56 +0800

    Revert "nvim: drop lazyvim"
    This reverts commit 7ca6ad499b203c99d054384b79e1cac2430bfd9f.

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -8,7 +8,7 @@ if status is-interactive
     abbr --add -- v vim
     abbr --add -- gi lazygit
 
-    alias vim="nvim"
+    alias vim="NVIM_APPNAME=lazyvim nvim"
     alias hx=helix
     alias ls=eza
     alias tree="eza --tree"
